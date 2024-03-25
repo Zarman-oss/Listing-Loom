0cda71802f922839b08303155a88fd2093d75149
z31430158@gmail.com <zarmanimran@gmail.com>
2024-03-25 09:52:54 +0500

    created layout routes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
+import Link from 'next/link.js';
 export default function page() {
   return (
     <div>
-      <h1>csac</h1>
+      <h1 className="text-2xl ">Welcome</h1>
+      <Link href="/properties">Show All Listings</Link>
     </div>
   );
 }
---
+++ app/properties/page.jsx
@@ -0,0 +1,8 @@
+export default function page() {
+  console.log(he);
+  return (
+    <div>
+      <h1 className="text-2xl ">Properties </h1>
+    </div>
+  );
+}
