92a7c9b58e9d63ec554984086a75e0aa602f89bb
z31430158@gmail.com <zarmanimran@gmail.com>
2024-03-28 19:04:22 +0500

    added margin to search input

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,28 +1,30 @@
+import HeroBtn from './heroBtn.jsx';
+
 export default function Hero() {
   return (
-    <div className="bg-white text-black py-20 flex items-center">
-      <div className="lg:w-1/2 text-left">
-        <h1 className="text-5xl font-bold text-red-700">
-          Search through countless of properties and rentals to match your needs
+    <div className="bg-white text-black py-20 w-full flex flex-col sm:flex-row justify-center items-center">
+      <div className="flex flex-col m-8 items-center ">
+        <h1 className="text-5xl font-bold text-wrap text-red-700 text-center">
+          Search Through Countless Properties And Rentals To Match Your Needs
         </h1>
-        <p className="mt-4 text-lg">
+        <p className="mt-4 text-xl  text-gray-700 text-wrap">
           Search, post rentals, and sell properties with ease
         </p>
       </div>
-      <div className="lg:w-1/2 flex justify-end items-center">
+
+      <div className="flex flex-col sm:flex-row items-center m-16 ">
         <input
           type="text"
-          placeholder="Search properties"
-          className="border border-gray-300 px-4 py-2 mr-2"
+          placeholder="Search City Zip Code"
+          className="border border-gray-300 px-4 py-2 mr-2 mb-2 sm:mb-0"
         />
-        <input
-          type="text"
-          placeholder="Search rentals"
-          className="border border-gray-300 px-4 py-2 mr-2"
-        />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Search
-        </button>
+        <select className="border border-gray-300 px-4 py-2 mr-2 mb-2 sm:mb-0">
+          <option value="house">House</option>
+          <option value="apartment">Apartment</option>
+          <option value="condo">Condo</option>
+          <option value="townhouse">Townhouse</option>
+        </select>
+        <HeroBtn />
       </div>
     </div>
   );
---
+++ components/heroBtn.jsx
@@ -0,0 +1,5 @@
+export default function heroBtn() {
+  return (
+    <button className="bg-red-700 text-white px-4 py-2 rounded">Search</button>
+  );
+}
